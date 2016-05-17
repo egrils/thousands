0c4154d3a66f54f281053f376fda480a8a2f13d1
liuyilin <335889390@qq.com>
2016-05-17 14:33:46 +0800

    create readme.md

--- src/separators.js
+++ src/separators.js
@@ -1,32 +1,72 @@
 'use strict';
 
 function thousands_separators(num) {
-    var number;
+    var numArray,apartNum,accountNum,text;
+
+    numArray = buildNumArray(num);
+    apartNum = buildApartNum(numArray);
+    accountNum = buildAccountNum(apartNum);
+    text = buildText(accountNum);
+
+    return text;
+}
+
+function buildNumArray(num) {
+    var numArray;
+
+    numArray = num.toString().split("");
+
+    return numArray;
+}
+
+function buildApartNum(numArray) {
+    var flag = 0;
+    var front = [];
+    var behind = [];
+
+    numArray.forEach(function (element) {
+        if (element == ".")
+            flag = 1;
+        if (flag == 0)
+            front.push(element);
+        else
+            behind.push(element);
+    });
+
+    return {Front:front,Behind:behind};
+}
+
+function buildAccountNum(apartNum) {
+    var flag = 0,count = -1,i = 0;
+    var accountFront = [];
+    var front = apartNum.Front;
+
+    front.forEach(function (element) {
+        count --;
+        accountFront.push(element);
+        if (flag == front.length % 3 || count == 0){
+            accountFront.splice(flag+i,0,",");
+            count = 3;
+            i ++;
+        }
+        flag ++;
+    });
+
+    if (front.length % 3 == 0)
+        accountFront.splice(0,1);
+    return {Front:accountFront,Behind:apartNum.Behind};
+}
+
+function buildText(accountNum) {
     var text = "";
 
-    if(num == 100)
-        number = num.toString().split("");
-    else if(num == 1000){
-        number = num.toString().split("");
-        number.splice(1,0,",");
-    }
-    else if(num == 1000000){
-        number = num.toString().split("");
-        number.splice(1,0,",");
-        number.splice(5,0,",");
-    }
-    else if(num == 1000.0){
-        number = num.toString().split("");
-        number.splice(4,2);
-    }
-    else if(num == 100.2342){
-        number = num.toString().split("");
-    }
-
-    number.forEach(function (element) {
+    accountNum.Front.forEach(function (element) {
         text += element;
     });
 
+    accountNum.Behind.forEach(function (element) {
+        text += element;
+    });
     return text;
 }
 
